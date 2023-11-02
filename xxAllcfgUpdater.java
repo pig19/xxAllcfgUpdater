a3c0dfa81108a51e9ccf04ce800345d5f69a61af
pig19 <pigtan.pigpig@gmail.com>
2023-11-02 23:35:02 +0900

    Rename file

--- xxAllcfgUpdater.java
+++ xxAllcfgUpdater.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class xxAllUpdater {
+public class xxAllcfgUpdater {
     public static void main(String[] args) {
         //使用するファイル名を定義
         String inputFileName = "BlockAndItemWithMetaNames.txt";
